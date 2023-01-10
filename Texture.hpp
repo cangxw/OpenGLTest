2145a06652769b93660b93efd7accd5940267d11
cangxw <cangxw@msn.com>
2023-01-11 00:09:42 +0800

    纹理文件
    纹理主类

---
+++ Texture.hpp
@@ -0,0 +1,29 @@
+//
+//  Texture.hpp
+//  OpenGLTest
+//
+//  Created by 仓潇伟 on 2023/1/10.
+//
+
+#pragma once
+
+#include "Renderer.h"
+
+class Texture
+{
+private:
+    unsigned int m_RendererID;
+    std::string m_FilePath;
+    unsigned char* m_LocalBuffer;
+    int m_Width, m_Height, m_BPP;
+    
+public:
+    Texture(const std::string& path);
+    ~Texture();
+    
+    void Bind(unsigned int slot = 0) const;
+    void Unbind() const;
+    
+    inline int GetWidth() const { return m_Width; }
+    inline int GetHeight() const { return m_Height; }
+};
---
+++ vendor/stb_image/stb_image.cpp
@@ -0,0 +1,9 @@
+//
+//  stb_image.cpp
+//  OpenGLTest
+//
+//  Created by 仓潇伟 on 2023/1/10.
+//
+
+#define  STB_IMAGE_IMPLEMENTATION
+#include "stb_image.hpp"
